c94474271ec4360ffef35854a333aa6a6b8ce965
Colyn Pacaldo <pacaldo.colyn@gmail.com>
2025-08-08 23:22:44 +0800

    Create BeforeClass_Codes.java

---
+++ CSIT221 DSA (C++)/BeforeClassNotes(Aug.8)/BeforeClass_Codes.java
@@ -0,0 +1,34 @@
+//Before Class (Aug.8, 2025)
+
+//1. Lesson 1 Topic 1 Arrays
+/*
+1. What advantage do arrays offer in terms of data access?
+  - Efficient storage and access
+2. What is the index range for the first and last element of an array?
+  - 0 to arraySize - 1
+3. What is the importance of arrays in loops and iterations?
+  - Facilitating iteration and operation execution
+4. What is the versatility of arrays in programming?
+  - Support for diverse data types
+5. Why are arrays foundational in algorithms, data structures, and programming practices?
+  - They facilitate efficient data storage and manipulation
+6. What is the purpose of arrays in programming?
+  - Grouping data
+7. How are array elements usually accessed?
+  - Using indices
+8. What advantage do arrays offer in terms of data organization?
+  - Facilitating the organization of related data items into a coherent structure
+9. How do arrays enhance iteration and processing?
+  - By streamlining iterative processes and efficiently handling sizable datasets
+10. What is the flexibility of arrays?
+  - Dynamic memory allocation techniques enable resizing to cater to varying data volumes
+*/
+
+//2. Lesson 1 Topic 2 One Dimensional Arrays
+
+
+//3. Lesson 1 Topic 3 Multi Dimensional Arrays
+
+
+//4. Lesson 1 Topic 4 Array Traversal
+
