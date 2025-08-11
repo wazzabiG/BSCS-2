5af0c8c29511bf0a11b7ac99718a89d816b6e73c
Colyn Pacaldo <pacaldo.colyn@gmail.com>
2025-08-12 03:43:09 +0800

    Update (Aug.11)Functions&MathLibrary.java

--- CSIT227 OOP (Java)/(Aug.11)Functions&MathLibrary.java
+++ CSIT227 OOP (Java)/(Aug.11)Functions&MathLibrary.java
@@ -1,2 +1,5 @@
-// (Aug.11, 2025)
-// Lesson 
+// Activities: 4 Functions & 1 Built-in Math Library (Aug.11, 2025)
+// 5 items each, 10 points per item
+
+// Lesson 10: Topic 5 (Functions with no parameters and return values)
+
